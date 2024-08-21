c6ddd7761ca94da3514a6eaa99e05078a7f93e41
veeramanikanta18.github.io <manikantadev2@gmail.com>
2024-08-21 20:01:34 +0530

    update app2

--- app2.py
+++ app2.py
@@ -13,7 +13,7 @@ st.write("""
 
 st.header("Enter DNA sequence")
 
-sequence_input  = ">DNA Query\n Enter your data"
+sequence_input  = ">DNA Query\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
 
 sequence = st.text_area("Sequence input", sequence_input, height=250)
 sequence = sequence.splitlines()
@@ -33,3 +33,42 @@ sequence
 
 st.header("OUTPUT (DNA Nucleotide Count)")
 
+st.subheader('1.Print dictionary')
+def DNA_nucleotide_count(seq):
+    d = dict([
+        ('A',seq.count('A')),
+        ('T',seq.count('T')),
+        ('G',seq.count('G')),
+        ('C',seq.count('C'))
+    ])
+
+    return d
+
+X = DNA_nucleotide_count(sequence)
+X_label = list(X)
+X_values = list(X.values())
+X
+
+## display dataframe
+
+st.subheader('Display dataframe')
+df = pd.DataFrame.from_dict(X, orient='index')
+df = df.rename({0:'count'}, axis='columns')
+df.reset_index(inplace=True)
+df = df.rename(columns={'index':'nucleotide'})
+st.write(df)
+
+### display Bar chart using altair
+
+st.subheader("Display Bar chart")
+
+p = alt.Chart(df).mark_bar().encode(
+    x='nucleotide',
+    y='count'
+)
+
+p = p.properties(
+    width = alt.Step(80) # controls width of bar
+)
+
+st.write(p)
