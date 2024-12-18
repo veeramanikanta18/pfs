07e44ced321b650af9ba55b6b8472e8f4ae49cb7
veeramanikanta18 <manikantadev2@gmail.com>
2024-12-18 20:41:54 +0530

    add file

---
+++ Ass_materials/Base_Program.py
@@ -0,0 +1,18 @@
+import sys
+from random import seed, randrange
+
+
+try:
+    arg_for_seed, upper_bound = (abs(int(x)) + 1 for x in input('Enter two integers: ').split())
+except ValueError:
+    print('Incorrect input, giving up.')
+    sys.exit()
+
+seed(arg_for_seed)
+mapping = {}
+for i in range(1, upper_bound):
+    r = randrange(-upper_bound // 8, upper_bound)
+    if r > 0:
+        mapping[i] = r
+print('\nThe generated mapping is:')
+print('  ', mapping)
